062357f70017ed78b3ad96e5cd196cb745986724
Graham Campbell <graham@mineuk.com>
2013-12-07 15:07:04 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,101 @@
+Laravel Navigation
+==================
+
+
+[![Bitdeli Badge](https://d2weczhvl823v0.cloudfront.net/GrahamCampbell/Laravel-Navigation/trend.png)](https://bitdeli.com/free "Bitdeli Badge")
+[![Build Status](https://travis-ci.org/GrahamCampbell/Laravel-Navigation.png?branch=master)](https://travis-ci.org/GrahamCampbell/Laravel-Navigation)
+[![Latest Version](https://poser.pugx.org/graham-campbell/navigation/v/stable.png)](https://packagist.org/packages/graham-campbell/navigation)
+[![Total Downloads](https://poser.pugx.org/graham-campbell/navigation/downloads.png)](https://packagist.org/packages/graham-campbell/navigation)
+[![Scrutinizer Quality Score](https://scrutinizer-ci.com/g/GrahamCampbell/Laravel-Navigation/badges/quality-score.png?s=00adc2bf1ad673660b1955e237fbf8ce7979dca2)](https://scrutinizer-ci.com/g/GrahamCampbell/Laravel-Navigation)
+[![Still Maintained](http://stillmaintained.com/GrahamCampbell/Laravel-Navigation.png)](http://stillmaintained.com/GrahamCampbell/Laravel-Navigation)
+
+
+## What Is Laravel Navigation?
+
+Laravel Navigation is a navigation bar generator for [Laravel 4](http://laravel.com).  
+
+* Laravel Navigation was created by, and is maintained by [Graham Campbell](https://github.com/GrahamCampbell).  
+* Laravel Navigation relies on my [Laravel HTMLMin](https://github.com/GrahamCampbell/Laravel-HTMLMin) package.  
+* Laravel Navigation uses [Travis CI](https://travis-ci.org/GrahamCampbell/Laravel-Navigation) to run tests to check if it's working as it should.  
+* Laravel Navigation uses [Scrutinizer CI](https://scrutinizer-ci.com/g/GrahamCampbell/Laravel-Navigation) to run additional tests and checks.  
+* Laravel Navigation uses [Composer](https://getcomposer.org) to load and manage dependencies.  
+* Laravel Navigation provides a [change log](https://github.com/GrahamCampbell/Laravel-Navigation/blob/master/CHANGELOG.md), [releases](https://github.com/GrahamCampbell/Laravel-Navigation/releases), and a [wiki](https://github.com/GrahamCampbell/Laravel-Navigation/wiki).  
+* Laravel Navigation is licensed under the Apache License, available [here](https://github.com/GrahamCampbell/Laravel-Navigation/blob/master/LICENSE.md).  
+
+
+## System Requirements
+
+* PHP 5.3.3+, 5.4+ or PHP 5.5+ is required.
+* You will need [Laravel 4](http://laravel.com) because this package is designed for it.  
+* You will need [Composer](https://getcomposer.org) installed to load the dependencies of Laravel Navigation.  
+
+
+## Installation
+
+Please check the system requirements before installing Laravel Navigation.  
+
+To get the latest version of Laravel Navigation, simply require it in your `composer.json` file.
+
+`"graham-campbell/navigation": "dev-master"`
+
+You'll then need to run `composer install` or `composer update` to download it and have the autoloader updated.
+
+You will need to register the [Laravel HTMLMin](https://github.com/GrahamCampbell/Laravel-HTMLMin) service provider before you attempt to load the Laravel Navigation service provider. Open up `app/config/app.php` and add the following to the `providers` key.
+
+`'GrahamCampbell\HTMLMin\HTMLMinServiceProvider'`
+
+Once Laravel Navigation is installed, you need to register the service provider. Open up `app/config/app.php` and add the following to the `providers` key.
+
+`'GrahamCampbell\Navigation\NavigationServiceProvider'`
+
+You can register the Navigation facade in the `aliases` key of your `app/config/app.php` file if you like.
+
+`'Navigation' => 'GrahamCampbell\Navigation\Facades\Navigation'`
+
+
+## Updating Your Fork
+
+The latest and greatest source can be found on [GitHub](https://github.com/GrahamCampbell/Laravel-Navigation).  
+Before submitting a pull request, you should ensure that your fork is up to date.  
+
+You may fork Laravel Navigation:  
+
+    git remote add upstream git://github.com/GrahamCampbell/Laravel-Navigation.git
+
+The first command is only necessary the first time. If you have issues merging, you will need to get a merge tool such as [P4Merge](http://perforce.com/product/components/perforce_visual_merge_and_diff_tools).  
+
+You can then update the branch:  
+
+    git pull --rebase upstream develop
+    git push --force origin <branch_name>
+
+Once it is set up, run `git mergetool`. Once all conflicts are fixed, run `git rebase --continue`, and `git push --force origin <branch_name>`.  
+
+
+## Pull Requests
+
+Please submit pull requests against the develop branch.  
+
+* Any pull requests made against the master branch will be closed immediately.  
+* If you plan to fix a bug, please create a branch called `fix-`, followed by an appropriate name.  
+* If you plan to add a feature, please create a branch called `feature-`, followed by an appropriate name.  
+* Please indent with 4 spaces rather than tabs, and make sure your code is commented.  
+
+
+## License
+
+Apache License  
+
+Copyright 2013 Graham Campbell  
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at  
+
+ http://www.apache.org/licenses/LICENSE-2.0  
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.  
---
+++ src/GrahamCampbell/Navigation/Classes/Navigation.php
@@ -0,0 +1,203 @@
+<?php namespace GrahamCampbell\Navigation\Classes;
+
+/**
+ * This file is part of Laravel Navigation by Graham Campbell.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @package    Laravel-Navigation
+ * @author     Graham Campbell
+ * @license    Apache License
+ * @copyright  Copyright 2013 Graham Campbell
+ * @link       https://github.com/GrahamCampbell/Laravel-Navigation
+ */
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\UrlGenerator;
+use Illuminate\Config\Repository;
+use GrahamCampbell\HTMLMin\Classes\HTMLMin;
+
+abstract class Navigation {
+
+    /**
+     * The items in the main nav bar.
+     *
+     * @var array
+     */
+    protected $main = array();
+
+    /**
+     * The items in the bar nav bar.
+     *
+     * @var array
+     */
+    protected $bar = array();
+
+    /**
+     * The request instance.
+     *
+     * @var \Illuminate\Http\Request
+     */
+    protected $request;
+
+    /**
+     * The url instance.
+     *
+     * @var \Illuminate\Routing\UrlGenerator
+     */
+    protected $url;
+
+    /**
+     * The config instance.
+     *
+     * @var \Illuminate\Config\Repository
+     */
+    protected $config;
+
+    /**
+     * The htmlmin instance.
+     *
+     * @var \GrahamCampbell\HTMLMin\Classes\HTMLMin
+     */
+    protected $htmlmin;
+
+    /**
+     * Create a new instance.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Routing\UrlGenerator  $url
+     * @param  \Illuminate\Config\Repository  $config
+     * @param  \GrahamCampbell\HTMLMin\Classes\HTMLMin  $htmlmin
+     * @return void
+     */
+    public function __construct(Request $request, UrlGenerator $url, Repository $config, HTMLMin $htmlmin) {
+        $this->request = $request;
+        $this->url = $url;
+        $this->config = $config;
+        $this->htmlmin = $htmlmin;
+    }
+
+    public function getMain($type = 'default') {
+        // fire event that can be hooked to add items to the nav bar
+        $this->event->fire('navigation.main', array(array('type' => $type)));
+
+        // check if the type exists in the main array
+        if (!array_key_exists($type, $this->main)) {
+            // add it if it doesn't exists
+            $this->main[$type] = array();
+        }
+
+        // apply active keys
+        $nav = $this->active($this->main[$type]);
+
+        // fix up and spit out the nav bar
+        return $this->process($nav);
+    }
+
+    public function getBar($type = 'default') {
+        // fire event that can be hooked to add items to the nav bar
+        $this->event->fire('navigation.bar', array(array('type' => $type)));
+
+        // check if the type exists in the bar array
+        if (!array_key_exists($type, $this->bar)) {
+            // add it if it doesn't exists
+            $this->bar[$type] = array();
+        }
+
+        // don't apply active keys
+        $nav = $this->bar[$type];
+
+        // fix up and spit out the nav bar
+        return $this->process($nav);
+    }
+
+    public function addMain($type = 'default', array $item, $first = false) {
+        // check if the type exists in the main array
+        if (!array_key_exists($type, $this->main)) {
+            // add it if it doesn't exists
+            $this->main[$type] = array();
+        }
+
+        // check if we are forcing the item to the start
+        if ($first) {
+            // add the item to the start of the array
+            $this->main[$type] = array_merge($this->main[$type], array($item));
+        } else {
+            // add the item to the end of the array
+            $this->main[$type][] = $item;
+        }
+    }
+
+    public function addBar($type = 'default', array $item, $first = false) {
+        // check if the type exists in the bar array
+        if (!array_key_exists($type, $this->bar)) {
+            // add it if it doesn't exists
+            $this->bar[$type] = array();
+        }
+
+        // check if we are forcing the item to the start
+        if ($first) {
+            // add the item to the start of the array
+            $this->bar[$type] = array_merge($this->bar[$type], array($item));
+        } else {
+            // add the item to the end of the array
+            $this->bar[$type][] = $item;
+        }
+    }
+
+    public function getHTML($maintype = 'default', $bartype = 'default', array $data = array('title' => 'Navigation', 'side' => 'dropdown', 'inverse' => true)) {
+        // get the nav bar arrays
+        $mainnav = $this->getMain($type);
+        $barnav = $this->getMain($type);
+
+        // return the html nav bar
+        return $this->htmlmin->view($this->config['navigation::view'], array_merge($data, array('main' => $mainnav, 'bar' => $barnav)));
+    }
+
+    protected function active(array $nav) {
+        // check if each item is active
+        foreach ($nav as $key => $value) {
+            // check if it is local
+            if (isset($value['slug'])) {
+                // if the request starts with the slug
+                if ($this->request->is($value['slug']) || $this->request->is($value['slug'].'/*')) {
+                    // then the navigation item is active, or selected
+                    $nav[$key]['active'] = true;
+                } else {
+                    // then the navigation item is not active or selected
+                    $nav[$key]['active'] = false;
+                }
+            } else {
+                // then the navigation item is not active or selected
+                $nav[$key]['active'] = false;
+            }
+        }
+
+        // spit out the nav bar array at the end
+        return $nav;
+    }
+
+    protected function process(array $nav) {
+        // convert slugs to urls
+        foreach ($nav as $key => $value) {
+            // if the url is not set
+            if (!isset($value['url'])) {
+                // set the url based on the slug
+                $nav[$key]['url'] = $this->url->to($value['slug']);
+            }
+            // remove any remaining slugs
+            unset($nav[$key]['slug']);
+        }
+
+        // spit out the nav bar array at the end
+        return $nav;
+    }
+}
