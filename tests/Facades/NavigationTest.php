23d7d4b038b31c839e0d425bf775ac17887ab982
Graham Campbell <graham@alt-three.com>
2015-06-25 14:17:48 +0100

    Updated email addresses

--- tests/Facades/NavigationTest.php
+++ tests/Facades/NavigationTest.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of Laravel Navigation.
  *
- * (c) Graham Campbell <graham@cachethq.io>
+ * (c) Graham Campbell <graham@alt-three.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -17,7 +17,7 @@ use GrahamCampbell\Tests\Navigation\AbstractTestCase;
 /**
  * This is the navigation facade test class.
  *
- * @author Graham Campbell <graham@cachethq.io>
+ * @author Graham Campbell <graham@alt-three.com>
  */
 class NavigationTest extends AbstractTestCase
 {
